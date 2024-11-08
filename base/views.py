4a8c26b4fbdd7ad6a152c12283fd69d5cecd06b5
drishyalol <sdrishyaraj@gmail.com>
2024-11-08 14:42:38 +0545

    added nav bar both for pages and tabs

--- base/views.py
+++ base/views.py
@@ -5,10 +5,7 @@ from .models import Note, NoteCategory
 # Create your views here.
 def home(request):
     notesObjData = Note.objects.all() # returns a list of objects
-    data = {'notes': notesObjData} # dictionary with notes as key and notesObjData as value
+    noteCategoryObjs = NoteCategory.objects.all() # returns a list of objects
+    data = {'notes': notesObjData , 'notecategory': noteCategoryObjs} # dictionary with notes as key and notesObjData as value
     return render(request, "index.html", context = data) #context sends the data to the template
-
-def create(request):
-    notesCategoryData = NoteCategory.objects.all()
-    data2 = {'notesCategory': notesCategoryData}
-    return render(request, "create.html", context = data2)
+    
